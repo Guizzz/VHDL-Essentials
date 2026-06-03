86c0286037861419e9f74c40d10d85c3064b444f
Andrea Guizzaro <45075994+Guizzz@users.noreply.github.com>
2026-06-03 11:22:41 +0200

    refactor: consolidate file watchers in qsfViewService (#16)

--- src/services/qsfViewService.ts
+++ src/services/qsfViewService.ts
@@ -13,55 +13,12 @@ export async function registerQsfView(context: vscode.ExtensionContext)
         );
     
     const watcher = vscode.workspace.createFileSystemWatcher('**/*.{qsf,do,vhd}');
-    watcher.onDidCreate(
-        async () => {
-            await tabView.loadData();
-        }
-    );
-    
-    const createListener =
-        vscode.workspace.onDidCreateFiles(
-            async (docs: vscode.FileCreateEvent) => 
-            {   
-                for (const d of docs.files)
-                {   
-                    if (d.path.endsWith('.qsf') || d.path.endsWith('.do') || d.path.endsWith('.vhd'))
-                    {
-                       await tabView.loadData(); 
-                    }
-                }
-            }
-        );
-
-    const deleteListener =
-        vscode.workspace.onDidDeleteFiles(
-            async (docs: vscode.FileDeleteEvent) => 
-            {   
-                for (const d of docs.files)
-                {
-                    if (d.path.endsWith('.qsf') || d.path.endsWith('.do') || d.path.endsWith('.vhd'))
-                    {
-                       await tabView.loadData(); 
-                    }
-                }
-            }
-        );
-
-    const saveListener =
-        vscode.workspace.onDidSaveTextDocument(
-            async (doc) => 
-            {
-                if (doc.fileName.endsWith('.qsf') || doc.fileName.endsWith('.do') || doc.fileName.endsWith('.vhd')) 
-                {
-                    await tabView.loadData();
-                }
-            }
-        );
+    watcher.onDidCreate(() => tabView.loadData());
+    watcher.onDidChange(() => tabView.loadData());
+    watcher.onDidDelete(() => tabView.loadData());
 
     context.subscriptions.push(
         treeProvider,
-        saveListener,
-        createListener,
-        deleteListener
+        watcher
     );
 }
